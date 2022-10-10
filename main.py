4ee612ac3df93d8d77746b41290c5ba5645d54db
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-10-10 10:15:15 +0200

    a

--- main.py
+++ main.py
@@ -1,61 +1,49 @@
+
 class BaseAccount:
     def __init__(self, accountNo:str, interest:float):
         self.__AccountNo = accountNo
-        self.__Saldo = 0
+        self.__Balance = 0
         self.__Interest = interest
+
+    def GetSaldo(self)-> int:
+        return self.__Balance
+
+    def GetKontonummer(self)-> int:
+        return self.__AccountNo
+
+    def Deposit(self, amount:int):
+        amount = amount - 10
+        self.__Balance = self.__Balance + amount
+
 
 class SavingsAccount (BaseAccount): # Sparkonto
     def __init__(self, accountNo:str):
         super().__init__(accountNo, 1.0)
 
-    def GetSaldo(self)-> int:
-        return self.__Saldo
-
-    def GetKontonummer(self)-> int:
-        return self.__AccountNo
-
-    def Deposit(self, amount:int):
-        amount = amount - 10
-        self.__Saldo = self.__Saldo + amount
-
     def Withdraw(self, amount:int) -> bool:
         if amount > 10000:
             return False
-        if amount > self.__Saldo:
+        if amount > self.__Balance:
             return False
-        self.__Saldo = self.__Saldo - amount
+        self.__Balance = self.__Balance - amount
 
-
-
-class SalaryAccount: # Lönekonto
+class SalaryAccount (BaseAccount): # Lönekonto
     def __init__(self, accountNo:str):
-        self.__AccountNo = accountNo
-        self.__Saldo = 0
-        self.__Interest = 0.01
-
-    def GetSaldo(self)-> int:
-        return self.__Saldo
-
-    def GetKontonummer(self)-> int:
-        return self.__AccountNo
-
-    def Deposit(self, amount:int):
-        amount = amount - 10
-        self.__Saldo = self.__Saldo + amount
+        super().__init__(accountNo, 0.1)
 
     def Withdraw(self, amount:int) -> bool:
-        if amount > self.__Saldo:
+        if amount > self.__Balance:
             return False
-        self.__Saldo = self.__Saldo - amount
+        self.__Balance = self.__Balance - amount
 
 
 
 
 
 
-konto1 = SavingsAccount("121212")
-konto1.Deposit(200)
 konto2 = SavingsAccount("121213")
+konto1 = SalaryAccount("121212")
+konto1.Deposit(200)
 konto2.Deposit(100)
 print(f"{konto1.GetKontonummer()} : {konto1.GetSaldo()}")
 
